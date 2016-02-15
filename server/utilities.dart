b919d58c7a17179d6f0e57644d372a084974a727
Daniel Caaz <DCaazy@gmail.com>
2016-02-15 14:04:20 -0800

    Clean up utilities.

--- server/utilities.dart
+++ server/utilities.dart
@@ -1,14 +1,10 @@
 import "dart:io";
-import "dart:async";
+// import "dart:async";
 import "package:express/express.dart";
 import "package:jaded/jaded.dart" as jade;
 import "../jade.views.dart" deferred as views;
 
-// Fucking organize this file.
-
-// Init the page variable!
-Map pages = new Map();
-renderPage(HttpContext ctx,String page) => ctx..sendHtml(pages[page]())..end();
+renderPage(HttpContext ctx,String page) => ctx..sendHtml(views.JADE_TEMPLATES["views/pages/$page.jade"]())..end();
 class HybridFileHandler extends StaticFileHandler {
   String atPath;
   HybridFileHandler([this.atPath]);
@@ -25,29 +21,16 @@ class HybridFileHandler extends StaticFileHandler {
     else renderPage(ctx,'404');
   }
 }
-Future<Map> compile() async {
-  // File name regex. this as opposed to ./this.jade
-  RegExp fname = new RegExp(r'.+/(.+?)\.(?:.+?)$');
-  // Just check if the file name begins with an underscore. Probably doesn't work exactly as it should but who knows.
+compile() async {
+  RegExp fileName = new RegExp(r'.+/(.+?)\.(?:.+?)$');
   RegExp underscore = new RegExp(r'/_.+$');
-
-  // Now, let's compile our stylesheets.
+  // Compile stylesheets.
   for (var file in new Directory('./stylesheets/').listSync())
     if ((file is File) && (!underscore.hasMatch(file.path)))
-      // Run sass with scss syntax, compress it, and don't use a sourcemap.
       Process.run('sass', [ '--scss', '--style=compressed', '--sourcemap=none',
-        // Original file, output file.
-        file.path, './public/css/'+fname.firstMatch(file.path).group(1)+'.css'])
-        // If there was an error, throw it!
+        file.path, './public/css/'+fileName.firstMatch(file.path).group(1)+'.css'])
         ..then((proc){ if(proc.stderr.length>1) throw proc.stderr; });
-
-
-  // Compile our jade pages.
+  // Compile jade.
   new File('jade.views.dart').writeAsStringSync(jade.renderDirectory('views/pages/'));
-  // Load up our compiled pages.
-  await views.loadLibrary();
-  // Make keys less ridiculous by only using the file name.
-  for(String key in views.JADE_TEMPLATES.keys)
-    pages[fname.firstMatch(key).group(1)] = views.JADE_TEMPLATES[key];
-  return pages;
+  views.loadLibrary();
 }

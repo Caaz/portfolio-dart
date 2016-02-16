17bce8296d221f6182af594a6ac5b8de7707ab25
Daniel Caaz <DCaazy@gmail.com>
2016-02-15 16:03:47 -0800

    Rename and reorganize utilities, HybridFileHandler now.
    This change makes everything unified!

--- main.dart
+++ main.dart
@@ -1,12 +1,11 @@
-import "server/utilities.dart";
+import "server/HybridFileHandler.dart";
 import "package:express/express.dart";
 
-// If this file looks clean it just means there's a really messy file somewhere else.
-
 main() async {
-  await compile();
+  HybridFileHandler hybrid = new HybridFileHandler('public');
+  await hybrid.compileViews();
   new Express()
-  ..use(new HybridFileHandler('public'))
-  ..get('/', (ctx) => renderPage(ctx,'index'))
+  ..use(hybrid)
+  ..get('/', (ctx) => hybrid.renderView(ctx,'index'))
   ..listen('0.0.0.0', 80);
 }
